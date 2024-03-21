a08fd59863c8891a92edb7b3d0aa90ccf9869abd
MOA <149222183+Mohammedomair23@users.noreply.github.com>
2024-03-21 21:42:27 +0530

    NarrowCastex.java

---
+++ NarrowCastex.java
@@ -0,0 +1,12 @@
+public class NarrowCastex
+{
+public static void main(String[] args)
+{
+int marks=70,sub=30;
+double dmarks=(double)marks;
+short submarks=(short)sub;
+System.out.println(marks);
+System.out.println(dmarks);
+System.out.println(submarks);
+}
+}
