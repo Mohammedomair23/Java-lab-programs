3508ef1fb4b042ec1208f25f2f92cbdf7db9e325
MOA <149222183+Mohammedomair23@users.noreply.github.com>
2024-03-21 21:43:43 +0530

    subtraction in java

---
+++ Sub.java
@@ -0,0 +1,9 @@
+class Sub
+{
+public static void main(String[] args)
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.println("Subtraction of "+a+"-"+b+"is"+(a-b));
+}
+}
