9c1a6c09027c2991331740b3578ea2e1f7c637cb
MOA <149222183+Mohammedomair23@users.noreply.github.com>
2024-03-21 21:15:04 +0530

    Add files via upload
    Addition without using scanner

---
+++ Add2.java
@@ -0,0 +1,9 @@
+class add
+{
+public static void main(String[] args)
+{
+int  a=Integer.parseInt(args[0]);
+int  b=Integer.parseInt(args[1]);
+System.out.println("Addition of "+a+"+"+b+"is"+(a+b));
+}
+}
