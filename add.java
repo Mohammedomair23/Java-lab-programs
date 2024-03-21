2c62637a7b60e2d2b19fd3876dee30762c865525
MOA <149222183+Mohammedomair23@users.noreply.github.com>
2024-03-21 21:56:34 +0530

    Addition using scanf

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Add{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the values of a and b :");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("Addition of  "+a+" + "+b+" is"+(a+b));
+}
+}
