ecf88ccd4a4b31aa5ca1d80affa46b87ff4d0b17
MOA <149222183+Mohammedomair23@users.noreply.github.com>
2024-03-28 12:33:56 +0530

    Add files via upload

---
+++ IterArr.java
@@ -0,0 +1,17 @@
+class IterArr{
+    public static void main(String[] ar){
+        int[] arr={1,2,4,5,6,7};
+        ComArray co=new ComArray();
+        System.out.println("array 3 elements");
+        for(int i:co.arr3){
+            System.out.println(1);
+        }
+
+        ComArray.main(null);
+        System.out.println("elements are");
+        for(int i=0;i<arr.length;i++){
+
+        }
+
+    }
+}    
---
+++ Iterater.java
@@ -0,0 +1,12 @@
+class IterateArray{
+    public static void main(string[] ar){
+        int[] arr={1,2,3,4,5,6,7};
+        System.out.println("1");
+        System.out.println("elements are");
+        for(int i=0;i<arr.length;i++){
+            System.out println(arr[i]);
+        }
+    
+    }
+
+}
