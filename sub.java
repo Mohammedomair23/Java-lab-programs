a689f4508ba6594f9efd68feace878db19fcc1db
Abhishek <149222222+abhishekmahendarkar@users.noreply.github.com>
2024-03-30 00:16:12 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,6 @@
+class Add{
+               public static void main(String[] args) {
+                            int a=30,b=3;
+                            System.out.println("result is "+(a-b));
+               }
+}
